8d3865ad00915cffebf2faac2c03eb7de4ba2b74
Natasha Lie 李沁霞 <108567215+nalliee@users.noreply.github.com>
2025-05-01 23:16:41 +0800

    sql script solutions

---
+++ 01_Script/Medium/23_JPMorgan_Card Launch Success.sql
@@ -0,0 +1,18 @@
+-- JPMorgan Card Launch Success --
+
+WITH card_launch AS (
+  SELECT
+    card_name,
+    issued_amount,
+    MAKE_DATE(issue_year, issue_month, 1) issue_date,
+    MIN(MAKE_DATE(issue_year, issue_month, 1)) OVER (
+      PARTITION BY card_name) AS launch_date
+  FROM monthly_cards_issued
+)
+
+SELECT
+  card_name,
+  issued_amount
+FROM card_launch
+WHERE issue_date = launch_date
+ORDER BY issued_amount DESC;
