afebf565658b0a09d9e22a336e5b1d6155c8070f
Natasha Lie 李沁霞 <108567215+nalliee@users.noreply.github.com>
2025-05-07 00:05:15 +0800

    sql script solutions

---
+++ 01_Script/Easy/24_Visa_ApplePay_Volume.sql
@@ -0,0 +1,9 @@
+-- Visa ApplePay Volume --
+
+SELECT
+  merchant_id,
+  SUM(CASE WHEN LOWER(payment_method) = 'apple pay' THEN transaction_amount 
+    ELSE 0 END) AS total_transaction
+FROM transactions
+GROUP BY merchant_id
+ORDER BY total_transaction DESC;
